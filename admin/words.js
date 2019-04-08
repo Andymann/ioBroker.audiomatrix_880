1e9767492009000e6c29c5d13fab8f3258c4ad9d
Andy Fischer <info@andyland.info>
2019-04-08 18:11:27 +0200

    "Initial commit"

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'audiomatrix_880 adapter settings': {
+		'en': 'Adapter settings for audiomatrix_880',
+		'de': 'Adaptereinstellungen für audiomatrix_880',
+		'ru': 'Настройки адаптера для audiomatrix_880',
+		'pt': 'Configurações do adaptador para audiomatrix_880',
+		'nl': 'Adapterinstellingen voor audiomatrix_880',
+		'fr': "Paramètres d'adaptateur pour audiomatrix_880",
+		'it': "Impostazioni dell'adattatore per audiomatrix_880",
+		'es': 'Ajustes del adaptador para audiomatrix_880',
+		'pl': 'Ustawienia adaptera dla audiomatrix_880',
+		'zh-cn': 'audiomatrix_880的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
